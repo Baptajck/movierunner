af61ed61330b306a9fc587e6b9d4ba06258a001d
Baptajck <parville.baptiste@gmail.com>
2020-09-10 10:02:23 +0200

    Updates

---
+++ precache-manifest.b1e6c6682558a205d130b69dfab655fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245c1e4324691fa0657da41f9d2567cd",
+    "url": "/Baptajck/LikemovieV2/index.html"
+  },
+  {
+    "revision": "4efe85dfd60dc6e951d1",
+    "url": "/Baptajck/LikemovieV2/static/css/main.f8d52e19.chunk.css"
+  },
+  {
+    "revision": "ccf66748ff10f55d183a",
+    "url": "/Baptajck/LikemovieV2/static/js/2.cf95977f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Baptajck/LikemovieV2/static/js/2.cf95977f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4efe85dfd60dc6e951d1",
+    "url": "/Baptajck/LikemovieV2/static/js/main.425760db.chunk.js"
+  },
+  {
+    "revision": "60cc1d8c675c0ba201fb",
+    "url": "/Baptajck/LikemovieV2/static/js/runtime-main.ff77a595.js"
+  }
+]);
