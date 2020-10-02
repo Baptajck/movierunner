74862514d7a04789ef3731ca8737637f3131a605
Baptajck <parville.baptiste@gmail.com>
2020-10-02 11:00:38 +0200

    Updates

---
+++ precache-manifest.e44aaeb847ce46bef0709ded3681534c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73ccd2fce78b84ff07f6664db139ccb",
+    "url": "/movierunner/index.html"
+  },
+  {
+    "revision": "60e63edefca409ea27a7",
+    "url": "/movierunner/static/css/main.5425f3d8.chunk.css"
+  },
+  {
+    "revision": "3a5472fbbdcd1afdd0e3",
+    "url": "/movierunner/static/js/2.f810c6fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movierunner/static/js/2.f810c6fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e63edefca409ea27a7",
+    "url": "/movierunner/static/js/main.03a5312b.chunk.js"
+  },
+  {
+    "revision": "0e47776a0b220a82ea4d",
+    "url": "/movierunner/static/js/runtime-main.4192a6f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movierunner/precache-manifest.7d5eb0b21e01d8fac5d414ea99398f63.js"
+  "/movierunner/precache-manifest.e44aaeb847ce46bef0709ded3681534c.js"
 );
 
 self.addEventListener('message', (event) => {
